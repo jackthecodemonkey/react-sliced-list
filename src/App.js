f31c10200c8790f75e66cbe61e9e6455504c1092
Jack Nam <jack.nam@zuuse.com>
2019-07-28 22:28:26 +1000

    refactoring

--- src/App.js
+++ src/App.js
@@ -26,11 +26,11 @@ const gridWrapperStyle = (height) => {
 
 const getStyleProp = (str) => Number(str.replace('px', ''));
 
-class MagicGrid extends React.Component {
+class SmallScroll extends React.Component {
   constructor(props) {
     super(props);
     this.gridWrapper = React.createRef();
-    this.getStyleProps = this.getStyleProps.bind(this);
+    this.handleOnScroll = this.handleOnScroll.bind(this);
     this.wrapperRef = React.createRef();
     this.state = {
       start: 0,
@@ -51,39 +51,70 @@ class MagicGrid extends React.Component {
     return { height };
   }
 
-  getStyleProps() {
+  get maxVisibleRows() {
     const { height } = this.getWrapperStyle();
+    return Math.ceil(height / this.props.rowHeight);
+  }
+
+  rowStartIndex(startIndex) {
+    return startIndex > 2
+      ? startIndex - 1
+      : startIndex
+  }
+
+  rowEndIndex(startIndex, endIndex) {
+    return startIndex <= 2
+      ? endIndex + 1
+      : endIndex
+  }
+
+  handleOnScroll() {
     const currentScollTop = this.wrapperRef.current.scrollTop;
-    const { rowHeight } = this.props;
-    let maxVisibleRows = Math.ceil(height / rowHeight);
     let startIndex = 0;
     let endIndex;
-    let currentIndex = Math.floor(currentScollTop / rowHeight);
+    let currentIndex = Math.floor(currentScollTop / this.props.rowHeight);
 
     if (currentIndex > 0) startIndex = currentIndex
-    endIndex = maxVisibleRows + currentIndex;
+    endIndex = this.maxVisibleRows + currentIndex;
 
     this.setState({
-      start: startIndex,
-      end: endIndex,
+      start: this.rowStartIndex(startIndex),
+      end: this.rowEndIndex(startIndex, endIndex),
       scrollTop: currentScollTop
     })
   }
 
-  ren(r) {
-    return <div style={{ display: 'flex', flexDirection: 'column' }}>{r}</div>;
+  renderChildren(rows) {
+    let first = [];
+    if (this.state.start) {
+      let rest = Math.floor(this.state.scrollTop / this.props.rowHeight);
+      let offset = 0;
+      let lastPage = this.props.totalRows - this.maxVisibleRows;
+      if (this.endIndex > lastPage) {
+        offset = 1;
+      }
+      for (let i = 0; i < rest - offset; i++) {
+        first.push(<div style={{ height: `${this.props.rowHeight}px` }}></div>);
+      }
+      return first.concat(rows);
+    }
+    const rowsToRender = first.length
+      ? first
+      : rows;
+    if (!rowsToRender.length) return null;
+    return <div style={{ display: 'flex', flexDirection: 'column' }}>{rowsToRender}</div>;
   }
 
   render() {
     return (
       <div
         ref={this.wrapperRef}
-        onScroll={this.getStyleProps}
+        onScroll={this.handleOnScroll}
         style={wrapperStyle}>
         <div ref={this.gridWrapper} style={gridWrapperStyle(this.props.totalRows * this.props.rowHeight)}>
           {
             this.state.end > 0 &&
-            this.ren(this.props.children(this.state.start, this.state.end, this.state.scrollTop))
+            this.renderChildren(this.props.children(this.state.start, this.state.end, this.state.scrollTop))
           }
         </div>
       </div>
@@ -117,60 +148,28 @@ class Row extends React.Component {
 }
 
 function App() {
-  const arr = Array(4000);
-  for (let i = 0; i < 4000; i++) {
+  const arr = Array(2000);
+  for (let i = 0; i < 2000; i++) {
     arr[i] = i;
   }
 
-  const getRows = (start, end, scrollTop) => {
-    if (start === -1 || end === -1) return null;
-
-    let startIndex = start;
-    let endIndex = end;
-    let offset = 0;
-
-    if (start > 2) {
-      startIndex = start - 1;
-    } else {
-      endIndex = end + 1;
-    }
-
-    if (end > 3980) {
-      offset = 1;
-    }
-
-    const slice = arr.slice(
-      startIndex,
-      endIndex
-    );
-
-    if (slice && slice.length) {
-      const tmp = slice.map((num) => {
-        return <Row number={num} />
-      })
-
-      if (start) {
-        let first = [];
-        let rest = Math.floor(scrollTop / 40);
-        for (let i = 0; i < rest - offset; i++) {
-          first.push(<div style={{ height: '40px' }}></div>);
-        }
-        return first.concat(tmp);
-      }
-      return tmp;
-    }
+  const getRows = (start, end) => {
+    const slice = arr.slice(start, end);
+    return slice.map((num) => {
+      return <Row number={num} />
+    })
   }
 
   return (
     <div className="App">
       <div style={{ marginTop: '50px' }}>
-        <MagicGrid
-          totalRows={4000}
+        <SmallScroll
+          totalRows={2000}
           rowHeight={40}>
-          {(start, end, scrollTop) => {
-            return getRows(start, end, scrollTop)
+          {(start, end) => {
+            return getRows(start, end)
           }}
-        </MagicGrid>
+        </SmallScroll>
       </div>
     </div>
   );
